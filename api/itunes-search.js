1d869401fc39770974c6b1179e285ff7bcbc1ec0
Alonso7ac <63118342+Alonso7ac@users.noreply.github.com>
2025-08-10 02:55:26 -0600

    Create itunes-search.js

---
+++ api/itunes-search.js
@@ -0,0 +1,29 @@
+// /api/itunes-search.js
+
+export default async function handler(req, res) {
+  try {
+    const {
+      q = '',
+      genreId,
+      limit = '20',
+      country = 'US',
+      playable = 'true'
+    } = req.query;
+
+    const params = new URLSearchParams();
+    params.set('country', country);
+    params.set('media', 'music');
+    params.set('entity', 'song');
+    params.set('limit', String(limit || 20));
+    if (q) params.set('term', q);
+    if (genreId) params.set('genreId', String(genreId));
+
+    const url = `https://itunes.apple.com/search?${params.toString()}`;
+    const r = await fetch(url);
+    const data = await r.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
